b43fc2a02bda7f2e1383a4dbb0542b27496d05e7
Jam Toast <joao.jqn@gmail.com>
2026-01-26 15:06:15 +0100

    Added pausing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
     public static bool isInside;
+    public static bool paused = false;
+
     private GameObject interior;
     private GameObject exterior;
     private TaskManager tm;
     private TimeController tc;
     private PlayerStats stats;
+
+    private InputSystem_Actions uiControls;
+    private InputAction pauseAction;
 
 
     public static int stage;
@@ -29,6 +35,12 @@ public class GameManager : MonoBehaviour
             stats.HasWon = true;
         }
 
+        if (pauseAction.WasPressedThisFrame())
+        {
+            paused = !paused;
+        }
+        int pauseInt = paused ? 1 : 0;
+        Time.timeScale = pauseInt;
 
         if (interior != null && interior != null) {
             if (isInside)
@@ -41,6 +53,29 @@ public class GameManager : MonoBehaviour
                 exterior.SetActive(true);
             }
         }
+    }
+
+
+    private void Awake()
+    {
+        // Initialize input system
+        uiControls = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        // Get the look action and enable it
+        pauseAction = uiControls.UI.Pause;
+        pauseAction.Enable();
+        
+        // Enable the player action map
+        uiControls.UI.Enable();
+    }
+
+    private void OnDisable()
+    {
+        pauseAction.Disable();
+        uiControls.UI.Disable();
     }
 
 }
