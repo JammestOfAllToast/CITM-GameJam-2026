239bcb6d55e1e5c6f72940280c010c7ef78d08bd
Jam Toast <joao.jqn@gmail.com>
2026-01-27 16:13:49 +0100

    fixed crash

--- Assets/Scripts/WatchManager.cs
+++ Assets/Scripts/WatchManager.cs
@@ -2,16 +2,22 @@ using UnityEngine;
 
 public class WatchManager : MonoBehaviour
 {
+    private bool closed;
     void OnEnable()
     {
         GetComponent<Animator>().Play("WatchAnimation", -1, 1f);
+        closed = false;
     }
 
     void Update()
     {
-       // GetComponent<Animator>().speed = -1f;
-     //   GetComponent<Animator>().Play("WatchAnimation", -1, 1f);
-      //  Invoke("Deactivate", GetComponent<Animation>().clip.length);
+        if (!GameManager.inventoryOpen && !closed)
+        {
+            closed = true;
+            GetComponent<Animator>().speed = -1f;
+            GetComponent<Animator>().Play("WatchAnimation", -1, 1f);
+            Invoke("Deactivate", GetComponent<Animation>().clip.length);
+        }
     }
 
     void Deactivate()
